fae72f767fccf8d208e9545bcecf30d7d4eac31c
LewisHendy2605 <lewishendy26@gmail.com>
2024-07-18 23:43:43 +0100

    readded scaling

--- Overworld.js
+++ Overworld.js
@@ -79,7 +79,7 @@ class OverWorld {
     this.startGameLoop();
 
     this.map.startCutscene([
-      //{ type: "battle" },
+      { type: "battle" },
       //{ type: "changeMap", map: "DemoRoom" },
       //{ type: "textMessage", text: "Hello Buddy, your in the kitchen now !!" },
     ]);
