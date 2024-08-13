82bc9ddf9a18ca53b8e08ca0071886f47c24a38f
LewisHendy2605 <lewishendy26@gmail.com>
2024-08-13 22:07:54 +0100

    readded walles removed for testing + removed miss pixel from console

--- OverWorldMap.js
+++ OverWorldMap.js
@@ -489,9 +489,9 @@ window.OverworldMaps = {
 
       // Right rail
       [utils.asGridCoord(59, 47)]: true,
-      // [utils.asGridCoord(59, 48)]: true,
-      // [utils.asGridCoord(59, 49)]: true,
-      // [utils.asGridCoord(59, 50)]: true,
+      [utils.asGridCoord(59, 48)]: true,
+      [utils.asGridCoord(59, 49)]: true,
+      [utils.asGridCoord(59, 50)]: true,
       [utils.asGridCoord(59, 51)]: true,
 
       // Bottom right rail
