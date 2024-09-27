95dd56cae721b87b7a527cebb388123d06272e9d
LewisHendy2605 <lewishendy26@gmail.com>
2024-09-27 22:45:28 +0100

    fix to upper image error

--- OverWorldMap.js
+++ OverWorldMap.js
@@ -461,7 +461,7 @@ window.OverworldMaps = {
   Tardis: {
     id: "Tardis",
     lowerSrc: "/images/tardis/Tardis-map-v7.png",
-    upperSrc: "",
+    upperSrc: "/images/maps/KitchenUpper.png",
     outsideMap: "Outside_tardis",
     tardisLanded: true,
     gameObjects: {
