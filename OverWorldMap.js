cb7805bb4f9d1b4e4f5ecd32ac7ef7a0a2734bb1
LewisHendy2605 <lewishendy26@gmail.com>
2024-09-29 19:39:57 +0100

    small console upade + isometric floor for entrance

--- OverWorldMap.js
+++ OverWorldMap.js
@@ -474,7 +474,7 @@ window.OverworldMaps = {
       console: new Console({
         x: utils.withGrid(47.5),
         y: utils.withGrid(48),
-        src: "/images/tardis/console-v9.png",
+        src: "/images/tardis/console-v10.png",
         isConsole: true,
         behaviorLoop: [
           //{ type: "circleLeverDown" }
